4fedf17d9b188b60d1e5fb122e8f69c7a6ed2c09
ounique <believedream95@gmail.com>
2018-12-04 14:31:41 +0300

    Remaping user class

--- src/main/java/com/vsu/project/service/entity/User.java
+++ src/main/java/com/vsu/project/service/entity/User.java
@@ -8,34 +8,34 @@ import java.sql.Timestamp;
 
 @Data
 @Entity
-@Table(name = "test")
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-//
-//    @Column(name = "student_book_id")
-//    private long studentBookId;
-//
-//    @Column(name = "department_id")
-//    private long departmentId;
+
+    @Column(name = "student_book_id")
+    @NotNull
+    private long studentBookId;
+
+    @Column(name = "department_id")
+    @NotNull
+    private long departmentId;
 
     @NotNull
     private String username;
-//
-//    @Column(name = "description")
-//    private String description;
+
+    private String description;
 
     @NotNull
     private String password;
-//
-//    @Column(name = "rating")
-//    private long rating;
-//
-//    @Column(name = "birthday")
-//    private Timestamp birthday;
-//
-//    @Column(name = "picture")
-//    private String picture;
+
+    @NotNull
+    private long rating;
+
+    @NotNull
+    private Timestamp birthday;
+
+    private String picture;
 }
